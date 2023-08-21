af870614f6c2e782ae402f789ccb576d297c6d40
NazarTrupach <112543920+NazarTrupach@users.noreply.github.com>
2023-08-21 13:51:45 +0300

    In this case I worked with Query Parameters

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ inventory = {
 }
 
 
+# Path parameters
 @app.get("/get-item/{item_id}/")
 def get_item(item_id: int = Path(..., description="The Api for your gods", gt=0, le=5)):
     return inventory[item_id]
+
+
+# Query Parameters
+@app.get("/get-by-name/{item_id}")
+def get_item(*, item_id: int, name: Optional[str] = None, test: int):
+    for item_id in inventory:
+        if inventory[item_id]['name'] == name:
+            return inventory[item_id]
+    return {"Data": "Not found"}
