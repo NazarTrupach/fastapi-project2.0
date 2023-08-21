5d7e5bc2500e0e3fe8dc5a87f8b938f578d8d11f
NazarTrupach <112543920+NazarTrupach@users.noreply.github.com>
2023-08-21 11:43:00 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+inventory = {
+    1: {
+        "name": "Milk",
+        "price": 3.99,
+        "brand": "Regular"
+    }
+}
+
+
+@app.get("/get-item/{item_id}/{name}")
+def get_item(item_id: int, name: str):
+    return inventory[item_id]
